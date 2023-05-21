e402c5186ec09f32e13b466eb876ac010248bb90
pedrazzimariana@gmail.com <pedrazzimariana@gmail.com>
2023-05-21 19:19:03 +0000

    conclui dim_regiao e descritivo

--- models/stage/erp/stg_erp__endereco.sql
+++ models/stage/erp/stg_erp__endereco.sql
@@ -10,7 +10,7 @@ with
             , cast(addressline1 as string) as endereco					
             , cast(addressline2 as string) as complemento_endereco					
             , cast(city as string) as cidade			
-            , cast(stateprovinceid	as int) as fk_estado					
+            , cast(stateprovinceid as int) as fk_estado					
             , cast(spatiallocation as string) as localizacao_espacial																						    
     from fonte_endereco
 )
